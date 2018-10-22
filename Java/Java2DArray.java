16564a668f80638a225d2b51fbdfa95598752f03
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2018-10-22 21:13:45 +0200

    Create Java2DArray.java

---
+++ Java/Java2DArray.java
@@ -0,0 +1,41 @@
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static int maximumValue(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        int width = arr[0].length - 2;
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = 0; j < width; j++) {
+                max = Math.max(max,
+                        arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+                        arr[i + 1][j + 1] +
+                        arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
+            }
+        }
+        return max;
+    }
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }            
+        scanner.close();
+        int v=maximumValue(arr);
+        System.out.println(v);
+    }
+}
