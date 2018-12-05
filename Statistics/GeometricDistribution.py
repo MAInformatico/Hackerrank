e1efa8e7cc2b60008acb59b1625574d8dc86008b
mike <mike@Freedom>
2018-12-05 20:42:05 +0100

    adding directory

--- Statistics/GeometricDistribution.py
+++
@@ -1,5 +0,0 @@
-a , b = input().split()
-prob =int(a)/int(b)
-inspc = int(input())
-
-print(round((1-prob)**(inspc-1)*prob,3))
--- Statistics/StandardDeviation.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-data = [int(x) for x in input().strip().split()]
-print(round((sum([(x-(sum(data) / n))**2 for x in data])/n)**.5, 1))
