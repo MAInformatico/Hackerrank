cc08aa24bd45ffb99489973ed1340b7d2ad1fa4d
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2019-02-05 19:51:03 +0100

    Create TwoStrings.py

---
+++ Python/TwoStrings.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def twoStrings(s1, s2):
+    return 'YES' if any(i in s2 for i in s1 ) else 'NO' 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
