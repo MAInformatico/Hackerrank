a01f62518e14bc12a7b2bb62ed646d3d7f99a60f
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-09-10 07:03:55 +0200

    Update TaumandBday.js

--- ProblemSolving/Algorithms/TaumandBday.js
+++ ProblemSolving/Algorithms/TaumandBday.js
@@ -35,7 +35,7 @@ function readLine() {
  */
 
 function taumBday(b, w, bc, wc, z) {
-    return b*Math.min(bc, wc+z) + w*Math.min(wc,bc+z);
+    return BigInt(b) * BigInt(Math.min(bc, wc+z)) + BigInt(w) * BigInt(Math.min(wc, bc+z));
 }
 
 function main() {
