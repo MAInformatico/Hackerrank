ce3ce68fea59e52e0b7ec4978b24d16993fdddd2
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2020-09-14 18:31:23 +0200

    Create BeautifulTriplets.js

---
+++ ProblemSolving/Algorithms/BeautifulTriplets.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the beautifulTriplets function below.
+function beautifulTriplets(d, arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.includes(arr[i] + d) && arr.includes(arr[i] + d * 2)) result++        
+    }
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nd = readLine().split(' ');
+
+    const n = parseInt(nd[0], 10);
+
+    const d = parseInt(nd[1], 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = beautifulTriplets(d, arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
