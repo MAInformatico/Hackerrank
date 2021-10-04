3728355baca5501054b96de171c4b58cad27dd85
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-10-04 07:29:23 +0200

    Create CastleGrid.js

---
+++ ProblemSolving/DataStructures/CastleGrid.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumMoves' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY grid
+ *  2. INTEGER startX
+ *  3. INTEGER startY
+ *  4. INTEGER goalX
+ *  5. INTEGER goalY
+ */
+
+function minimumMoves(grid, startX, startY, goalX, goalY) {
+    if (grid[startX][startY] === 'X' || grid[goalX][goalY] === 'X') return 0;
+    const queue = [];
+    const initial = {
+        position: { x: startX, y: startY },
+        moves: 0
+    }
+    queue.push(initial);
+    
+    const visited = {} //Map
+    visited[`${startX}_${startY}`] = true;
+    
+    const markCellAsVisited = (x, y, moves, parent) => {
+        const cellKey = `${x}_${y}`
+        if (grid[x][y] === 'X' || visited[cellKey]) return null;
+        visited[cellKey] = true;
+        const newCell = {
+            position: { x, y },
+            moves,
+            parent
+        }
+        queue.push(newCell);
+        return newCell;
+    }
+    
+    while (queue.length > 0) {
+        const currentCell = queue.shift();
+        if (currentCell.position.x === goalX && currentCell.position.y === goalY){
+            return currentCell.moves;
+        }
+        const { position } = currentCell;
+        const moves = currentCell.moves + 1;
+        for (let y = position.y - 1; y >= 0; y--) { //Left
+            if (!markCellAsVisited(position.x, y, moves, currentCell)) break;
+        }
+        for (let x = position.x - 1; x >= 0; x--) { //Top
+            if (!markCellAsVisited(x, position.y, moves, currentCell)) break;
+        }
+        for (let y = position.y + 1; y < grid.length; y++) { //Right
+            if (!markCellAsVisited(position.x, y, moves, currentCell)) break;
+        }
+        for (let x = position.x + 1; x < grid.length; x++) { //Bottom
+            if (!markCellAsVisited(x, position.y, moves, currentCell)) break;
+        }
+    }
+    return 0;    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let grid = [];
+
+    for (let i = 0; i < n; i++) {
+        const gridItem = readLine();
+        grid.push(gridItem);
+    }
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const startX = parseInt(firstMultipleInput[0], 10);
+
+    const startY = parseInt(firstMultipleInput[1], 10);
+
+    const goalX = parseInt(firstMultipleInput[2], 10);
+
+    const goalY = parseInt(firstMultipleInput[3], 10);
+
+    const result = minimumMoves(grid, startX, startY, goalX, goalY);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
