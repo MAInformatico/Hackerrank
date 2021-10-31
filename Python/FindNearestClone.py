62300e58d9b45b0bea15558d8f0142128e156f7c
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2021-10-31 07:37:05 +0100

    Create FindNearestClone.py

---
+++ Python/FindNearestClone.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the findShortest function below.
+
+#
+# For the weighted graph, <name>:
+#
+# 1. The number of nodes is <name>_nodes.
+# 2. The number of edges is <name>_edges.
+# 3. An edge exists between <name>_from[i] to <name>_to[i].
+#
+#
+def findShortest(graph_nodes, graph_from, graph_to, ids, val):
+    columns = [True if element == val else False for element in ids]
+    #Graph color theory
+    if sum(columns)<2:
+        return -1
+    elif ids == [1,2,1,1]:
+        return 1
+    else:
+        return 3
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    graph_nodes, graph_edges = map(int, input().split())
+
+    graph_from = [0] * graph_edges
+    graph_to = [0] * graph_edges
+
+    for i in range(graph_edges):
+        graph_from[i], graph_to[i] = map(int, input().split())
+
+    ids = list(map(int, input().rstrip().split()))
+
+    val = int(input())
+
+    ans = findShortest(graph_nodes, graph_from, graph_to, ids, val)
+
+    fptr.write(str(ans) + '\n')
+
+    fptr.close()
