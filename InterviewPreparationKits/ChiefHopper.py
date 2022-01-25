fd88cbec4f5fff14ca94cdb31863032c42a37481
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2022-01-25 06:46:44 +0100

    Create ChiefHopper.py

---
+++ InterviewPreparationKits/ChiefHopper.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+
+#
+# Complete the 'chiefHopper' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def chiefHopper(arr):
+    result = 0
+
+    for i in reversed(arr):
+        result = math.ceil((result + i) / 2)
+
+    return result
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = chiefHopper(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
